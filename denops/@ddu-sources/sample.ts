1ef58b6ad043ad352c9b4eaea20dd1e2e5343f2d
kamecha <masakikameyama81@gmail.com>
2024-03-02 22:04:55 +0900

    treeで表示するitemの定義をした

--- denops/@ddu-sources/sample.ts
+++ denops/@ddu-sources/sample.ts
@@ -4,6 +4,37 @@ import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.10.2/types.ts";
 
 export type Params = Record<never, never>;
 
+type Tree = { text: string; children?: Tree[] };
+
+const sample: Tree = {
+  text: "root",
+  children: [
+    { text: "a" },
+    { text: "b" },
+    {
+      text: "c",
+      children: [
+        { text: "ca" },
+        { text: "cb" },
+      ],
+    },
+    { text: "d" },
+    {
+      text: "e",
+      children: [
+        {
+          text: "ea",
+          children: [
+            { text: "eaa" },
+            { text: "eab" },
+          ],
+        },
+        { text: "eb" },
+      ],
+    },
+  ],
+};
+
 export class Source extends BaseSource<Params> {
   gather(args: GatherArguments<Params>): ReadableStream<Item<unknown>[]> {
     return new ReadableStream({
